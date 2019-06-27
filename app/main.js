c41fa1cd1072734cbfd0080197417bf6f659a5b5
a-soliman <ahmed.soliman@programmer.net>
2019-06-27 08:14:46 -0700

    coded create window method

--- app/main.js
+++ app/main.js
@@ -1,3 +1,24 @@
 const { app, BrowserWindow, dialog } = require('electron');
 const path = require('path');
 const fs = require('fs');
+
+const windows = new Set();
+
+let createWindow = (exports.createWindow = () => {
+  let x, y;
+  const currentWindow = BrowserWindow.getFocusedWindow();
+
+  if (currentWindow) {
+    const [currentWindowX, currentWindowY] = currentWindow.getPosition();
+    x = currentWindowX + 20;
+    y = currentWindowY + 20;
+  }
+
+  let newWindow = new BrowserWindow({
+    x,
+    y,
+    show: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
