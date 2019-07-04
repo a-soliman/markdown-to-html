266aabd78bc1f2b76c47bc63c1adc4c2f2cb9e1b
a-soliman <ahmed.soliman@programmer.net>
2019-07-04 11:16:12 -0700

    fixied indintion

--- app/client/js/renderer.js
+++ app/client/js/renderer.js
@@ -4,50 +4,49 @@ const marked = require('marked');
 const ui = require('./ui');
 
 class App {
-    constructor(ui, ipcRenderer) {
-        this.ui = ui;
-        this.ipcRenderer = ipcRenderer;
-        this.init();
-    }
+  constructor(ui, ipcRenderer) {
+    this.ui = ui;
+    this.ipcRenderer = ipcRenderer;
+    this.init();
+  }
 
-    init = () => {
-        this.initEventListeners();
-        this.initIpcRendererListeners();
-    }
+  init = () => {
+    this.initEventListeners();
+    this.initIpcRendererListeners();
+  };
 
-    initEventListeners = () => {
-        const selectors = this.ui.selectors;
-        selectors.markdownView.addEventListener('keyup', this.renderHtml);
-        selectors.openFileBtn.addEventListener('click', this.getFileFromUser);
-        selectors.newFileBtn.addEventListener('click', this.launchNewWindow);
-    }
+  initEventListeners = () => {
+    const selectors = this.ui.selectors;
+    selectors.markdownView.addEventListener('keyup', this.renderHtml);
+    selectors.openFileBtn.addEventListener('click', this.getFileFromUser);
+    selectors.newFileBtn.addEventListener('click', this.launchNewWindow);
+  };
 
-    initIpcRendererListeners = () => {
-        this.ipcRenderer.on('file-opened', (evt, file, content) => {
-            this.ui.markdown = content;
-            this.renderHtml();
-        });
-    }
+  initIpcRendererListeners = () => {
+    this.ipcRenderer.on('file-opened', (evt, file, content) => {
+      this.ui.markdown = content;
+      this.renderHtml();
+    });
+  };
 
-    markdownToHtml = markdown => {
-        return marked(markdown, { senitize: true });
-    }
+  markdownToHtml = markdown => {
+    return marked(markdown, { senitize: true });
+  };
 
-    renderHtml = () => {
-        const markdown = this.ui.markdown;
-        const html = this.markdownToHtml(markdown);
-        return this.ui.renderHtml(html);
-    }
+  renderHtml = () => {
+    const markdown = this.ui.markdown;
+    const html = this.markdownToHtml(markdown);
+    return this.ui.renderHtml(html);
+  };
 
-    getFileFromUser = () => {
-        const currentWindow = remote.getCurrentWindow();
-        mainProcess.getFileFromUser(currentWindow);
-    }
+  getFileFromUser = () => {
+    const currentWindow = remote.getCurrentWindow();
+    mainProcess.getFileFromUser(currentWindow);
+  };
 
-    launchNewWindow = () => {
-        return mainProcess.createWindow();
-    }
+  launchNewWindow = () => {
+    return mainProcess.createWindow();
+  };
 }
-
 
 const app = new App(ui, ipcRenderer);
