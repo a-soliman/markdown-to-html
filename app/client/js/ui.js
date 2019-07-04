1eb3263a10b93fd3fd10a32261523e1efcc6dd33
a-soliman <ahmed.soliman@programmer.net>
2019-07-04 08:16:45 -0700

    coded ui class

---
+++ app/client/js/ui.js
@@ -0,0 +1,32 @@
+class UI {
+    constructor() {
+        this.selectors = {
+            controlsSection: document.querySelector('.controls'),
+            openFileBtn: document.querySelector('#open_file'),
+            newFileBtn: document.querySelector('#new_file'),
+            saveMarkdownBtn: document.querySelector('#save_markdown'),
+            revertBtn: document.querySelector('#revert'),
+            saveHTMLBtn: document.querySelector('#save_html'),
+            showFileBtn: document.querySelector('#show_file'),
+            openInDefaultBtn: document.querySelector('#open_in_default'),
+            contentSection: document.querySelector('.content'),
+            markdownView: document.querySelector('#markdown'),
+            htmlView: document.querySelector('#html')
+        };
+    }
+
+    get markdown() {
+        return this.selectors.markdownView.value;
+    }
+
+    set markdown(val) {
+        this.selectors.markdownView.value = val;
+    }
+
+    renderHtml = html => {
+        this.selectors.htmlView.innerHTML = html;
+    }
+}
+
+const ui = new UI();
+module.exports = ui;
