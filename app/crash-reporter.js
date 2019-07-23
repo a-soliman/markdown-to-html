21b8f12941d6d66547057767f773c572cebeea8d
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-07-23 08:54:41 -0700

    coded uncaught errors handler

--- app/crash-reporter.js
+++ app/crash-reporter.js
@@ -1,5 +1,8 @@
 const { crashReporter } = require('electron');
+const request = require('request');
 const host = 'http://localhost:3000';
+const manifest = require('../package.json');
+
 const config = {
   productName: 'Fire Sale',
   companyName: 'Electron in Action',
@@ -8,6 +11,32 @@ const config = {
 };
 
 crashReporter.start(config);
+
+const sendUncaughtException = error => {
+  const { productName, companyName } = config;
+  request.post(host + '/uncaughtexeptions', {
+    form: {
+      _productName: productName,
+      _companyName: companyName,
+      _version: manifest.version,
+      platform: process.platform,
+      process_type: process.type,
+      ver: process.versions.electron,
+      error: {
+        name: error.name,
+        message: error.message,
+        stack: error.stack
+      }
+    }
+  }).then(() => console.log('Sent Report'));
+};
+
+if (process.type === 'browser') {
+  process.on('uncaughtException', sendUncaughtException);
+} else {
+  window.addEventListener('error', sendUncaughtException);
+}
+
 console.log('[INFO] Crash reporting started.', crashReporter);
 
 module.exports = crashReporter;
